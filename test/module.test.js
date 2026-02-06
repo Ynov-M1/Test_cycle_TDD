7f80a9ccba345c12b68a0c7f2f3ba0a36950f3ce
LAFOND-Theo <theo03100@live.fr>
2026-02-06 12:13:48 +0100

    feat calculateAge: implement calculateAge with input validations

--- test/module.test.js
+++ test/module.test.js
@@ -19,12 +19,12 @@ describe('calculateAge Unit Test Suites', () => {
 
     it('should throw a "missing param p" error', () => {
         expect(() => calculateAge()).toThrow("missing param p")
+        expect(() => calculateAge(null)).toThrow("missing param p");
     })
 
     it('should throw "param p must be an object" if a non-object is passed', () => {
         expect(() => calculateAge("not an object")).toThrow("param p must be an object");
         expect(() => calculateAge(1)).toThrow("param p must be an object");
-        expect(() => calculateAge(null)).toThrow("param p must be an object");
     });
 
     it('should throw "missing birth field in param p" if birth is missing', () => {
