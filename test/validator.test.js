998a8d4d3f8e8923c575a97c568cb90dc4029d96
LAFOND-Theo <theo03100@live.fr>
2026-02-06 15:23:53 +0100

    test validator: add zip code validation tests

--- test/validator.test.js
+++ test/validator.test.js
@@ -1,11 +1,11 @@
-import { validatePerson } from "../src/validator";
+import {validatePerson, validateZipCode} from "../src/validator";
 import { validateAge } from "../src/validator";
 
 describe("validatePerson Unit Test Suites", () => {
     it("should throw INVALID_PERSON if param is missing or invalid", () => {
-        expect(() => validatePerson()).toThrow("INVALID_PERSON");
-        expect(() => validatePerson(null)).toThrow("INVALID_PERSON");
-        expect(() => validatePerson("not an object")).toThrow("INVALID_PERSON");
+        expect(() => validatePerson()).toThrow("MISSING_PARAM: person");
+        expect(() => validatePerson(null)).toThrow("MISSING_PARAM: person");
+        expect(() => validatePerson("not an object")).toThrow("MISSING_PARAM: person");
     });
 
     it("should validate a correct person object", () => {
@@ -35,5 +35,17 @@ describe("validateAge RED", () => {
     it("should accept adult", () => {
         const adult = new Date(new Date().getFullYear() - 20, 0, 1);
         expect(validateAge(adult)).toBe(true);
+    });
+});
+
+describe("validateZipCode RED", () => {
+    it("should throw INVALID_ZIP for invalid codes", () => {
+        ["1234", "ABCDE", null, "123456"].forEach(zip => {
+            expect(() => validateZipCode(zip)).toThrow("INVALID_ZIP");
+        });
+    });
+
+    it("should accept a valid code", () => {
+        expect(validateZipCode("03100")).toBe(true);
     });
 });
